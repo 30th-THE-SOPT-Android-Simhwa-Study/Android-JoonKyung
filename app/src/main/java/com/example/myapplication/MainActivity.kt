7446266573ea78250ddbe0bbedcc2542caaec12a
lee989898 <wnsrud1352@gmail.com>
2022-05-19 14:51:34 +0900

    [FEAT] 코루틴을 사용하여 원하는 회차 로또 당첨 번호 가져오기

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,98 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myapplication.databinding.ActivityMainBinding
+import com.google.gson.JsonParser
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.net.URL
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+
+        binding.generatebutton.setOnClickListener {
+
+            updateLottoBallImage(createLottoNumbers())
+
+            CoroutineScope(Dispatchers.IO).launch {
+                binding.tvWinning.text = getLottoNumbers().toString()
+            }
+        }
+    }
+    private fun createLottoNumbers(): ArrayList<Int> {
+        val result = arrayListOf<Int>()
+
+        val source = IntArray(45) { it + 1 }
+        val seed =
+            SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ", Locale.KOREA).format(Date()).hashCode()
+                .toLong()
+        val random = Random(seed)
+        source.shuffle(random)
+        source.slice(0..5).forEach { num ->
+            result.add(num)
+        }
+        result.sort()
+
+        return result
+    }
+
+    private fun getDrawableID(number: Int): Int {
+        val number = String.format("%02d", number)
+        val string = "ball_$number"
+        val id = resources.getIdentifier(string, "drawable", packageName)
+        return id
+    }
+
+    private fun updateLottoBallImage(result: ArrayList<Int>) {
+        with(binding) {
+            ivGame0.setImageResource(getDrawableID(result[0]))
+            ivGame1.setImageResource(getDrawableID(result[1]))
+            ivGame2.setImageResource(getDrawableID(result[2]))
+            ivGame3.setImageResource(getDrawableID(result[3]))
+            ivGame4.setImageResource(getDrawableID(result[4]))
+            ivGame5.setImageResource(getDrawableID(result[5]))
+        }
+    }
+
+    private suspend fun getLottoNumbers(): ArrayList<Int> {
+        val round = "1010"
+        val url = "https://dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=$round"
+        val lottoNumbers = ArrayList<Int>()
+
+        try {
+            val response = URL(url).readText()
+            val jsonObject = JsonParser.parseString(response).asJsonObject
+            val returnValue = jsonObject.get("returnValue").asString
+
+            if (returnValue == "success") {
+                for (i in 1..6) {
+                    val lottoNumber = jsonObject.get("drwtNo$i").asInt
+                    lottoNumbers.add(lottoNumber)
+                }
+                val bonusNumber = jsonObject.get("bnusNo").asInt
+                lottoNumbers.add(bonusNumber)
+                lottoNumbers.add(round.toInt())
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        return lottoNumbers
     }
 }
+
+
+
+// job 객체를 선언하여 Activity가 종료(destory) 될 때 수행 중인 job이 있다면 취소해주기
