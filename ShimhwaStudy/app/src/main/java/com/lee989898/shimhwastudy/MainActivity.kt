7884d1fe66b7e3a27054b459c5c306ff81741b51
mdb1217 <wnsrud1352@gmail.com>
2022-08-22 20:21:04 +0900

    [chore] 프로젝트 기초 세팅

---
+++ ShimhwaStudy/app/src/main/java/com/lee989898/shimhwastudy/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.lee989898.shimhwastudy
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
