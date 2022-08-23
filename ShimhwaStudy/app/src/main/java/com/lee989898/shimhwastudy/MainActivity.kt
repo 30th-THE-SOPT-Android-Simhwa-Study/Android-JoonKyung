5c3f7bdc116823c6019d1d57df6f53ca4b706da4
mdb1217 <wnsrud1352@gmail.com>
2022-08-23 19:52:14 +0900

    [feat] 메인액티비티 구현

--- ShimhwaStudy/app/src/main/java/com/lee989898/shimhwastudy/MainActivity.kt
+++ ShimhwaStudy/app/src/main/java/com/lee989898/shimhwastudy/MainActivity.kt
@@ -8,4 +8,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
 }
