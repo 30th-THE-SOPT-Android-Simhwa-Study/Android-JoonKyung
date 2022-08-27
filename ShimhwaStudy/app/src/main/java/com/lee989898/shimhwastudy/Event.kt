eadbf95a76033566360937913a0e2f15a46ebf39
mdb1217 <wnsrud1352@gmail.com>
2022-08-27 20:26:11 +0900

    [feat] Event 유틸 추가

---
+++ ShimhwaStudy/app/src/main/java/com/lee989898/shimhwastudy/Event.kt
@@ -0,0 +1,18 @@
+package com.lee989898.shimhwastudy
+
+class Event<out T>(private val content: T) {
+
+    var hasBeenHandled = false
+        private set
+
+    fun getContentIfNotHandled(): T? {
+        return if (hasBeenHandled) {
+            null
+        } else {
+            hasBeenHandled = true
+            content
+        }
+    }
+
+    fun peekContent(): T = content
+}
